a57e74668d44d41049ce461eb2e05ddc6af4cf22
Pagehey <pierregabriel@me.com>
2020-04-28 12:01:15 +0200

    module#4E11 done

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,3 +1,22 @@
+const mongoose = require('mongoose');
+const Store    = mongoose.model('Store');
+const params   = require('params');
+
 exports.homePage = (req, res) => {
   res.render('index');
+};
+
+exports.newStore = (req, res) => {
+  res.render('stores/new');
+};
+
+exports.createStore = async (req, res) => {
+  const store = new Store(req.storeParams);
+  await store.save();
+  res.redirect('/');
+};
+
+exports.storeParams = (req, res, next) => {
+  req.storeParams = params(req.body.store).only(['name', 'description', 'tags']);
+  next();
 };
--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router  = express.Router();
 
+const { catchErrors } = require('../handlers/errorHandlers');
+
 // controller imports
 const storeController = require('../controllers/storeController');
 
 router.get('/', storeController.homePage);
+router.get('/add', storeController.newStore);
+router.post('/add', storeController.storeParams, catchErrors(storeController.createStore));
+
 
 module.exports = router;
